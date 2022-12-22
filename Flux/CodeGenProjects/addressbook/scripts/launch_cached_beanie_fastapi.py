031fdeb1a771e28b9b86502b632d4b068df32f14
Ayra <soumyarai2050@gmail.com>
2022-12-22 12:20:29 +0800

    up-port

--- Flux/CodeGenProjects/addressbook/scripts/launch_cached_beanie_fastapi.py
+++ Flux/CodeGenProjects/addressbook/scripts/launch_cached_beanie_fastapi.py
@@ -12,11 +12,11 @@ env_dict = {
     "LOG_FILE_PATH": f"{project_dir / 'output' / 'logs.log'}",
     "LOG_LEVEL": "debug"
 }
-code_gen_engine_env_manager.init_env_and_update_sys_path("pair_strat_engine", "_", "_", env_dict)
+code_gen_engine_env_manager.init_env_and_update_sys_path("addressbook", "_", "_", env_dict)
 
 # Importing here to get LOG_FILE_PATH and LOG_LEVEL set before getting logging config triggered in
 # strat_manager_service_cached_beanie_fastapi file
-from Flux.CodeGenProjects.pair_strat_engine.output.strat_manager_service_cached_beanie_fastapi import \
+from Flux.CodeGenProjects.addressbook.output.strat_manager_service_cached_beanie_fastapi import \
     launch_strat_manager_service_cached_beanie_fastapi
 
 launch_strat_manager_service_cached_beanie_fastapi()
