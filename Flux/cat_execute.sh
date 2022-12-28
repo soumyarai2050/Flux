d0aa997cfe22c6b9950ce6cfdfcca0262b9c26f5
Ayra <soumyarai2050@gmail.com>
2022-12-28 19:29:19 +0800

    non editor set-up script

---
+++ Flux/cat_execute.sh
@@ -0,0 +1,2 @@
+#cd .. && exit 1 ; find . -name "*.sh" | xargs dos2unix && exit 1 ; find . -name "*.py" | xargs dos2unix && exit 1 ; export PYTHONPATH=$PWD:$PYTHONPATH && exit 1 ; cd -
+cd .. ; find . -name "*.sh" | xargs dos2unix ; find .pwd -name "*.py" | xargs dos2unix ; export PYTHONPATH=$PWD:$PYTHONPATH ; cd - ; cd Flux/CodeGenProjects ; ./add_project.sh pair_strat_engine addressbook with_model
